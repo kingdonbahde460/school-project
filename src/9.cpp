96e6229981146c8cfb337e2218ea286d26494619
kingdonbahde460 <kingdonbahde460@gmail.com>
2025-03-16 07:13:05 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  cout << "Hello world!" << endl;
+  string name = "Alice";
+  int age = 30;
+  cout << "My name is " << name << " and I am " << age << " years old." << endl;
+  return 0;
+}
