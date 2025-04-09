62111b4d7d74d3208cdc07970660be67d8f519ab
kingdonbahde460 <kingdonbahde460@gmail.com>
2025-04-09 21:18:17 +0000

    Add 37.cpp

---
+++ src/37.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    string name = "John Doe";
+    float height = 1.75f;
+
+    cout << "Age: " << age << endl;
+    cout << "Name: " << name << endl;
+    cout << "Height: " << height << endl;
+
+    return 0;
+}
