1c38689161864fa56f4dee4c89bb8ca2d041ed3a
kingdonbahde460 <kingdonbahde460@gmail.com>
2025-04-09 21:34:55 +0000

    Add 50.cpp

---
+++ src/50.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Positive number";
+    } else {
+        cout << "Not a positive number";
+    }
+    return 0;
+}
