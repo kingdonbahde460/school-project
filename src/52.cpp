87f907bf992b8fb60c157fc39084639057a4dc09
kingdonbahde460 <kingdonbahde460@gmail.com>
2025-04-09 21:37:21 +0000

    Add 52.cpp

---
+++ src/52.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int num = 5;
+    std::cout << "The value of num is: " << num << std::endl;
+
+    // Example loop that prints numbers from 1 to 5
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << i << " ";
+    }
+    return 0;
+}
