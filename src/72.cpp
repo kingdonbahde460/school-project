e073440f47b63603a9654aea8bb8f6c0caecb076
kingdonbahde460 <kingdonbahde460@gmail.com>
2025-05-25 11:56:33 +0000

    Add 72.cpp

---
+++ src/72.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5;
+    float f = 3.14f;
+    
+    cout << "i: " << i << endl;
+    cout << "f: " << f << endl;
+    
+    return 0;
+}
